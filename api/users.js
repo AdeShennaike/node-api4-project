8669a93e8a2cdd6f99d419657e4950b1d5ecf801
Ade <shennaike2386@hotmail.com>
2022-04-15 01:34:11 -0400

    finsihed API

---
+++ api/users.js
@@ -0,0 +1,7 @@
+const users = [
+    {name: "Adam Henry", password: 73864837 },
+    {name: "Clark Kent", password: 983323},
+    {name: "Mark Phillips", password: 2342344}
+]
+
+module.exports = users
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
+const users = require("./api/users")
 
 const server = express()
 
@@ -8,12 +9,32 @@ server.use(express.json())
 server.use(cors())
 server.use(helmet())
 
-server.get('/api/user', (req, res) => {
-    
-    res.status(200).json()
+server.get('/api/users', (req, res) => {
+    // throw new Error("whoops!")
+    try{
+        res.status(200).json(users)
+    }catch(err){
+        res.status(500).json({message: "yikes! not sure what went wrong >.<"})
+    }
 })
 
-server.post('/api/register', (req, res) => {})
-server.post('/api/login', (req, res) => {})
+server.post('/api/register', (req, res) => {
+    users.push(req.body)
+    try{
+        res.status(200).json(users[users.length - 1])
+    }catch(err){
+        res.status(500).json({message: "yikes! not sure what went wrong >.<"})
+    }
+})
+
+server.post('/api/login', (req, res) => {
+    if(req.body){
+        try{
+            res.status(200).json({message: "Welcome, I don't have to make you disappear, yay!"})
+        }catch(err){
+            res.status(500).json({message: "yikes! not sure what went wrong >.<"})
+        }
+    }
+})
 
 module.exports = server;
